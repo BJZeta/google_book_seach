6281921d39aa7d9dadb9192e1f501adb6f57682e
Brandon U <unzueta.brandon@gmai.com>
2019-10-09 20:15:29 -0700

    controller for book db

--- models/books.js
+++ models/books.js
@@ -6,7 +6,8 @@ const bookSchema = new Schema({
     title: { type: String, required: true },
     description: String,
     image: String,
-    link: { type: String, required: true }
+    link: { type: String, required: true },
+    date: { type: Date, default: Date.now }
 });
 
 const Book = mongoose.model('Book', bookSchema);
