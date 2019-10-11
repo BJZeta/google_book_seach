554b88ef7f839c9fd677d2f20f17a4e8ac66f1d2
Brandon U <unzueta.brandon@gmai.com>
2019-10-10 18:33:51 -0700

    adding Jumbotron and Navbar

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Jumbotron() {
+    return (
+        <div class="jumbotron bg-danger">
+            <div class="container">
+                <h1 class="display-4">(React) Google Book Search</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;
