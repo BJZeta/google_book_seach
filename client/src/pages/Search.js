bd3166bee072f7b7d0fa0ec126c9eed673d24a0a
Brandon U <unzueta.brandon@gmai.com>
2019-10-12 12:51:31 -0700

    working on Search page pt1

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
 import API from '../utils/API';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import Axios from 'axios';
 
 class Search extends Component {
     state = {
         books: [],
         title: '',
-        author: '',
-        description: '',
-        image: ''
+        hasSearched: false
     };
     
+    handleInputChange = e => {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleFormSubmit = e => {
+        e.preventDefault();
+
+        if (this.state.title) {
+            
+            const title = this.state.title;
+
+            API.bookSearch(title)
+            .then(results => {
+                
+                console.log(results.data.items);
+
+                this.setState({
+                    results: results.data.items,
+                    hasSearched: true
+                });
+            })
+            .catch(err => console.log(err));
+        };
+    };
+
+    render() { 
+        <form >
+            <input type="text"/>
+        </form>
+    }
+
 }
